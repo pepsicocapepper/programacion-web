a0831871870d0fe159d8985e00598cd2f9c74671
pepsicocapepper <snowproductions25@gmail.com>
2023-10-04 06:27:28 -0600

    Agregado el modulo ECMA y archivos miscelaneos

---
+++ Parcial2/ModuloECMA/computer_utilities.js
@@ -0,0 +1,6 @@
+//función que verifica si la computadora está encendida.
+export function isComputerOn(){
+	return true;
+}
+
+
---
+++ Parcial2/ModuloECMA/main.js
@@ -0,0 +1,2 @@
+import * as utils from './computer_utilities.js';
+console.log(utils.isComputerOn());
